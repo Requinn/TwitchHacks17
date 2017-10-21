9c035a31e631c609cc59e50cc166f68e32167e03
Kenny Do <contact@kennydo.com>
2017-10-21 10:13:43 -0700

    Emoji bar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import chatClient from './chatbot.js';
 import './App.css';
 import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip} from 'recharts';
+import twitchEmotes from './twitchEmotes.json';
 class App extends Component {
 
   constructor(props) {
@@ -12,20 +13,25 @@ class App extends Component {
       words: []
     };
     let client = new chatClient({
-        channel: '#riotgames',
+        channel: '#nightblue3',
         username: 'requinn',
         password: 'oauth:67hqi7y1taocu6knvw8sk33enwgaxe',
     });
     client.open();
     client.onMessageReceived = (message) => {
-      let newLog = this.state.log + " " + message.toLowerCase();
+      let newLog = this.state.log + " " + message;
       let map = this.wordcnt(newLog);
       let words = this.map_to_objarray(map);
+      let emoijiObjArray = words.filter((element) => {
+        const word = element.word;
+        return twitchEmotes[word] !== undefined;
+      });
       words.sort(function(a, b){return a.count - b.count});
       this.setState({
         log: newLog,
         map,
-        words
+        words,
+        emoijiObjArray
       });
     }
   }
@@ -44,16 +50,16 @@ class App extends Component {
     var a = [];
     var size = map.length;
     for(var m in map){
-      if(map[m] > 13){
+      if(map[m] > 1){
         a.push({word:m.substring(0), count:map[m]});
       }
     }
     return a;
   }
   	render () {
-      console.log(this.state.words)
+      console.log(this.state.emoijiObjArray)
     	return (
-      	<BarChart width={600} height={300} data={this.state.words}
+      	<BarChart width={600} height={300} data={this.state.emoijiObjArray}
               margin={{top: 5, right: 30, left: 20, bottom: 5}}>
          <XAxis dataKey="word"/>
          <YAxis/>
